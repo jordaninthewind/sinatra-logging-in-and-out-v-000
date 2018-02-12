3ad83c2dee7d9d4d928cd24639659f57f4cffa6f
Jordan Kline <jordan.kline@gmail.com>
2018-02-12 18:30:24 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,14 @@
-class Helpers
-  def current_user
+class Helpers #< User
+
+  def self.current_user(session)
+    User.find(session[:user_id])
     binding.pry
   end
 
-  def is_logged_in
+  def self.is_logged_in?(session)
+    binding.pry
+    session[:user_id] == User.id
+    !session.empty?
 
   end
 end
